bd75ec2db4cab42acaa1267cbd4ce7bf9ababf6b
MarwinPhilips <marwin.philips@gmx.ch>
2015-11-29 23:43:59 +0100

    no longer as singleton

--- src/main/java/ch/bfh/btx8081/w2015/black/MyMedicationApp/dataLayer/dataModel/MssqlEntityManager.java
+++ src/main/java/ch/bfh/btx8081/w2015/black/MyMedicationApp/dataLayer/dataModel/MssqlEntityManager.java
@@ -1,19 +1,20 @@
 package ch.bfh.btx8081.w2015.black.MyMedicationApp.dataLayer.dataModel;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import ch.bfh.btx8081.w2015.black.MyMedicationApp.dataLayer.dataInterfaces.MssqlEntityManagerInterface;
 
 public class MssqlEntityManager implements MssqlEntityManagerInterface {
-	
+
 	private static String PERSISTENCE_UNIT_NAME = "Medication";
-	private static EntityManager em = null;
-	public static EntityManager getEntityManager(){
-		if(em==null){
-			em= Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME).createEntityManager();
-		}
-		return em;
+
+	public static EntityManager getEntityManager() {
+		return Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME)
+				.createEntityManager();
+	}
+	public static EntityManager getEntityManager(String persistanceUnit) {
+		return Persistence.createEntityManagerFactory(persistanceUnit)
+				.createEntityManager();
 	}
 }
