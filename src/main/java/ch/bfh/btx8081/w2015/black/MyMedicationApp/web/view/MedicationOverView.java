f265251e143216f3c3996fccdba29b69b44ab088
ghodm1 <ghodm1@bfh.ch>
2015-12-02 02:18:56 +0100

    Now , this class extends NavigatorContainer class. Methods are not fully overridden.

--- src/main/java/ch/bfh/btx8081/w2015/black/MyMedicationApp/web/view/MedicationOverView.java
+++ src/main/java/ch/bfh/btx8081/w2015/black/MyMedicationApp/web/view/MedicationOverView.java
@@ -1,17 +1,21 @@
-package ch.bfh.btx8081.w2015.black.MyMedicationApp.web.view;
+	package ch.bfh.btx8081.w2015.black.MyMedicationApp.web.view;
 
+import com.vaadin.data.Item;
+import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.server.Resource;
 import com.vaadin.server.ThemeResource;
+import com.vaadin.server.VaadinService;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Embedded;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.VerticalLayout;
 
-public class MedicationOverView extends VerticalLayout implements View{
+public class MedicationOverView extends NavigatorContainer implements View{
 
 	private Table medicationTable;
-	//private VerticalLayout mainVerticalLayout;
 	private HorizontalLayout topHorizontalLayout;
 	private Button insertDrug;
 	private Button editDrug;
@@ -22,6 +26,14 @@ public class MedicationOverView extends VerticalLayout implements View{
 	 */
 	public MedicationOverView() {
 		super();
+		createTopHorizontalLayout();
+		createBodyVerticalLayout();
+		
+		addComponent(topHorizontalLayout);
+		addComponent(bodyVerticalLayout);
+		
+		
+		
 	}
 
 	/**
@@ -29,7 +41,10 @@ public class MedicationOverView extends VerticalLayout implements View{
 	 */
 	private void createTopHorizontalLayout(){
 		topHorizontalLayout=new HorizontalLayout();
-		
+		createButtons();
+		topHorizontalLayout.addComponent(insertDrug);
+		topHorizontalLayout.addComponent(editDrug);
+		topHorizontalLayout.addComponent(deleteDrug);
 	}
 	
 	/**
@@ -37,11 +52,15 @@ public class MedicationOverView extends VerticalLayout implements View{
 	 */
 	private void createBodyVerticalLayout(){
 		bodyVerticalLayout=new VerticalLayout();
+		createTable();
+		bodyVerticalLayout.addComponent(medicationTable);
 	}
 	/**
 	 * create three Buttons 
 	 */
 	private void createButtons(){
+		
+		
 		insertDrug=new Button("Insert", new ThemeResource("Icons/Insert.png"));
 		editDrug=new Button("Edit", new ThemeResource("Icons/Edit.png"));
 		deleteDrug=new Button("Delete", new ThemeResource("Icons/Delete.png"));
@@ -59,6 +78,16 @@ public class MedicationOverView extends VerticalLayout implements View{
 		medicationTable.addContainerProperty("Schema", String.class, null);
 		medicationTable.addContainerProperty("Note", String.class, null);
 		
+		Object newItemId = medicationTable.addItem();
+		Item row1 = medicationTable.getItem(newItemId);
+		row1.getItemProperty("Drug").setValue("Brufen");
+		row1.getItemProperty("Dose").setValue("400 mg");
+		row1.getItemProperty("Schema").setValue("1-1-1-0");
+		row1.getItemProperty("Note").setValue("Nach dem Essen");
+		
+		
+
+		
 	}
 	
 	
@@ -66,6 +95,44 @@ public class MedicationOverView extends VerticalLayout implements View{
 	public void enter(ViewChangeEvent event) {
 		// TODO Auto-generated method stub
 		
+		
+		
+	}
+
+	@Override
+	public String setNavBarTitle() {
+		// TODO Auto-generated method stub
+		return "Medication Over View";
+	}
+
+	@Override
+	public String setNavBarSubTitle() {
+		// TODO Auto-generated method stub
+		return "subtitle";
+	}
+
+	@Override
+	public String setNavBarHelpButtonText() {
+		// TODO Auto-generated method stub
+		return "Forward";
+	}
+
+	@Override
+	public String setNavBarMenuButtonText() {
+		// TODO Auto-generated method stub
+		return "Back";
+	}
+
+	@Override
+	public String setNavBarHelpButtonPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String setNavBarMenuButtonPath() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
