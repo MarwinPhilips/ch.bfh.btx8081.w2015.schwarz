ff3b1a32b206418ab729f0f23e49c8e1fc9d40e5
murbm1 <michelarthur.murbach@students.bfh.ch>
2016-01-14 14:05:07 +0100

    Prescription delete Test

---
+++ src/test/java/DeletePrescriptionTest.java
@@ -0,0 +1,45 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ch.bfh.btx8081.w2015.black.MyMedicationApp.businessLogic.model.Prescription;
+import ch.bfh.btx8081.w2015.black.MyMedicationApp.businessLogic.model.PrescriptionStates.PrescriptionContext;
+import ch.bfh.btx8081.w2015.black.MyMedicationApp.dataLayer.dataModel.MssqlEntityManager;
+import ch.bfh.btx8081.w2015.black.MyMedicationApp.dataLayer.dataModel.PrescriptionRepository;
+import ch.bfh.btx8081.w2015.black.MyMedicationApp.dataLayer.dataModel.Interfaces.IPrescriptionRepository;
+
+
+public class DeletePrescriptionTest {
+
+    	PrescriptionContext px = null;
+	MssqlEntityManager mem = null;
+	IPrescriptionRepository prescriptionRepo = null;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+	@Before
+	public void setUp() throws Exception {
+		mem = new MssqlEntityManager();
+		mem.contextInitialized(null);
+		px = new PrescriptionContext();
+		prescriptionRepo = new PrescriptionRepository();
+		
+	
+	}
+	
+	@After
+	public void destroy(){
+		mem.contextDestroyed(null);
+	}
+
+	@Test
+	public void test() {
+		Prescription prescription = px.getPrescription();
+		px.save();
+		px.delete();
+		Prescription deletetprescription = (Prescription) prescriptionRepo.getById(Prescription.class, prescription.getPrescriptionId());
+		deletetprescription.isDeleted();
+	}
+
+}
