60b73cd43ec7b5d08b5794fb1c0c7d3d35724b4f
Jonas Mosimann <jonasmosi@gmail.com>
2016-01-14 14:16:31 +0100

    added more tests for validation

--- src/test/java/ValidationTest.java
+++ src/test/java/ValidationTest.java
@@ -11,15 +11,22 @@ import com.vaadin.ui.AbstractSelect;
 import com.vaadin.ui.ComboBox;
 
 import ch.bfh.btx8081.w2015.black.MyMedicationApp.businessLogic.model.MethodOfApplication;
+import ch.bfh.btx8081.w2015.black.MyMedicationApp.businessLogic.model.WayOfApplication;
 import ch.bfh.btx8081.w2015.black.MyMedicationApp.dataLayer.dataModel.MethodOfApplicationRepository;
 import ch.bfh.btx8081.w2015.black.MyMedicationApp.dataLayer.dataModel.MssqlEntityManager;
+import ch.bfh.btx8081.w2015.black.MyMedicationApp.dataLayer.dataModel.WayOfApplicationRepository;
 
 public class ValidationTest {
 	
 	private MethodOfApplicationRepository methodOfApplicationRepository;
-	private BeanItemContainer<MethodOfApplication> container;
+	private WayOfApplicationRepository wayOfApplicationRepository;
+	
+	private BeanItemContainer<MethodOfApplication> containerMethodOfApplication;
+	private BeanItemContainer<WayOfApplication> containerWayOfApplication;
 	
 	private ComboBox methodOfApplicationComboBox;
+	private ComboBox wayOfApplicationComboBox;
+	
 	MssqlEntityManager mem = null;
 	
 	@Before
@@ -27,22 +34,41 @@ public class ValidationTest {
 		mem = new MssqlEntityManager();
 		mem.contextInitialized(null);
 		
+		setUpMethodOfApplication();
+		setUpWayOfApplication();
+	}
+	
+	public void setUpMethodOfApplication() {
 		methodOfApplicationComboBox = new ComboBox("Method of Application");
 		methodOfApplicationComboBox.addValidator(new NullValidator("you must select a method", false));
 		
 		methodOfApplicationRepository = new MethodOfApplicationRepository();
 		
-		container = new BeanItemContainer<MethodOfApplication>(MethodOfApplication.class, methodOfApplicationRepository.getAllMethodOfApplication());
-		methodOfApplicationComboBox.setContainerDataSource(container);
+		containerMethodOfApplication = new BeanItemContainer<MethodOfApplication>(MethodOfApplication.class, methodOfApplicationRepository.getAllMethodOfApplication());
+		methodOfApplicationComboBox.setContainerDataSource(containerMethodOfApplication);
 		methodOfApplicationComboBox.setItemCaptionMode(AbstractSelect.ItemCaptionMode.PROPERTY);
 		methodOfApplicationComboBox.setItemCaptionPropertyId("name");		
 		methodOfApplicationComboBox.setNullSelectionAllowed(false);
 		methodOfApplicationComboBox.setNewItemsAllowed(false);
 	}
+	
+	public void setUpWayOfApplication() {
+		wayOfApplicationComboBox = new ComboBox("Method of Application");
+		wayOfApplicationComboBox.addValidator(new NullValidator("you must select a method", false));
+		
+		wayOfApplicationRepository = new WayOfApplicationRepository();
+		
+		containerWayOfApplication = new BeanItemContainer<WayOfApplication>(WayOfApplication.class, wayOfApplicationRepository.getAllWayOfApplication());
+		wayOfApplicationComboBox.setContainerDataSource(containerWayOfApplication);
+		wayOfApplicationComboBox.setItemCaptionMode(AbstractSelect.ItemCaptionMode.PROPERTY);
+		wayOfApplicationComboBox.setItemCaptionPropertyId("name");		
+		wayOfApplicationComboBox.setNullSelectionAllowed(false);
+		wayOfApplicationComboBox.setNewItemsAllowed(false);
+	}
 
 	@Test
-	public void testIfApplicationIsSeleced() {
-		methodOfApplicationComboBox.select(container.firstItemId());
+	public void testIfMethodApplicationIsSeleced() {
+		methodOfApplicationComboBox.select(containerMethodOfApplication.firstItemId());
 		try {
 			methodOfApplicationComboBox.validate();
 		} catch (InvalidValueException e) {
@@ -52,13 +78,34 @@ public class ValidationTest {
 	}
 	
 	@Test
-	public void testIfApplicationIsNotSeleced() {
+	public void testIfMethodApplicationIsNotSeleced() {
 		try {
             methodOfApplicationComboBox.validate();
         } catch (InvalidValueException e) {
             return;
         }
 		fail("no selection in methodOfApplication is not working");
+	}
+	
+	@Test
+	public void testIfWayOfApplicationIsSeleced() {
+		wayOfApplicationComboBox.select(containerWayOfApplication.firstItemId());
+		try {
+			wayOfApplicationComboBox.validate();
+		} catch (InvalidValueException e) {
+			fail("InvalidValueException of wayOfApplication");
+		}
+		assertTrue("OK", wayOfApplicationComboBox.isValid());
+	}
+	
+	@Test
+	public void testIfWayOfApplicationIsNotSeleced() {
+		try {
+            wayOfApplicationComboBox.validate();
+        } catch (InvalidValueException e) {
+            return;
+        }
+		fail("no selection in wayOfApplication is not working");
 	}
 	
 	@After
