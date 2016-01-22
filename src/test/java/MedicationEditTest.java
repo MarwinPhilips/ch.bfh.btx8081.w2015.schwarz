5f88d4566afe8c20790610ec2ebd9787d2e0e0bc
turnm1 <turnamete@hotmail.com>
2016-01-22 11:58:22 +0100

    update and comment the MedicationEditTest

--- src/test/java/MedicationEditTest.java
+++ src/test/java/MedicationEditTest.java
@@ -1,6 +1,7 @@
 
 import static org.junit.Assert.assertTrue;
 
+import java.sql.Date;
 import java.util.GregorianCalendar;
 
 import org.junit.After;
@@ -37,62 +38,41 @@ public class MedicationEditTest {
 		mem.contextDestroyed(null);
 	}
 
-	@Test
-	public void checkStatePatternChangeToRunning() {
-		PrescriptionContext pc = new PrescriptionContext();
-		Prescription prescription = pc.getPrescription();
-		assertTrue("Staus nach Erstellung auf NEW",prescriptionRepo.getById(prescription.getPrescriptionId()).getPrescriptionState().equals(PrescriptionStateEnum.New));
-		assertTrue("Staus nach Erstellung auf NEW",prescription.getPrescriptionState().equals(PrescriptionStateEnum.New));
-		prescription.setReserveMedication(true);
-		assertTrue("OK",prescriptionRepo.getById(prescription.getPrescriptionId()).getPrescriptionState().equals(PrescriptionStateEnum.New));
-		assertTrue("OK",prescription.getPrescriptionState().equals(PrescriptionStateEnum.New));
-		pc.save();
-		assertTrue("Status nach Speicherung ist nicht Running",prescriptionRepo.getById(prescription.getPrescriptionId()).getPrescriptionState().equals(PrescriptionStateEnum.Running));
-		assertTrue("OK",prescription.getPrescriptionState().equals(PrescriptionStateEnum.Running));
-		//Clean up
-		pc.delete();	
-		prescriptionRepo.remove(prescriptionRepo.getById(Prescription.class, pc.getPrescription().getPrescriptionId()));
-	}
 	
 	@Test
-	public void checkStatePatternChangeToDeleted() {
-		PrescriptionContext pc = new PrescriptionContext();
-		Prescription prescription = pc.getPrescription();
-		assertTrue("Staus nach Erstellung auf NEW",prescriptionRepo.getById(prescription.getPrescriptionId()).getPrescriptionState().equals(PrescriptionStateEnum.New));
-		assertTrue("Staus nach Erstellung auf NEW",prescription.getPrescriptionState().equals(PrescriptionStateEnum.New));
-		prescription.setReserveMedication(true);
-		assertTrue("OK",prescriptionRepo.getById(prescription.getPrescriptionId()).getPrescriptionState().equals(PrescriptionStateEnum.New));
-		assertTrue("OK",prescription.getPrescriptionState().equals(PrescriptionStateEnum.New));
-		pc.save();
-		assertTrue("Status nach Speicherung ist nicht Running",prescriptionRepo.getById(prescription.getPrescriptionId()).getPrescriptionState().equals(PrescriptionStateEnum.Running));
-		assertTrue("OK",prescription.getPrescriptionState().equals(PrescriptionStateEnum.Running));
-		//Clean up
-		pc.delete();	
-		prescriptionRepo.remove(prescriptionRepo.getById(Prescription.class, pc.getPrescription().getPrescriptionId()));
-	}
-	
-	@Test
+	// Test Unit of editing of Prescription
 	public void checkStatePatternChangeToEditAndEnd(){
+		// Load data from prescription and test status of prescription = NEW
 		PrescriptionContext pc = new PrescriptionContext();
 		Prescription prescription = pc.getPrescription();
 		pc.setPrescription(prescription);
-		assertTrue("Staus vor Editieren auf Running",prescription.getPrescriptionState().equals(PrescriptionStateEnum.New));
+		assertTrue("Staus nach Erstellung ist nicht auf NEW",prescriptionRepo.getById(prescription.getPrescriptionId()).getPrescriptionState().equals(PrescriptionStateEnum.New));
+		assertTrue("Staus nach Erstellung ist nicht auf NEW",prescription.getPrescriptionState().equals(PrescriptionStateEnum.New));
+		prescription.setReserveMedication(true);
+		assertTrue("Staus vor Editieren ist nicht auf Running",prescriptionRepo.getById(prescription.getPrescriptionId()).getPrescriptionState().equals(PrescriptionStateEnum.New));
+		assertTrue("Staus vor Editieren ist nicht auf Running",prescription.getPrescriptionState().equals(PrescriptionStateEnum.New));
 		pc.save();
-		assertTrue("Staus vor Editieren auf Running",prescription.getPrescriptionState().equals(PrescriptionStateEnum.Running));
+		// after pc.save() should the status of prescription = Running
+		assertTrue("Status nach Speicherung ist nicht Running",prescriptionRepo.getById(prescription.getPrescriptionId()).getPrescriptionState().equals(PrescriptionStateEnum.Running));
+		assertTrue("OK",prescription.getPrescriptionState().equals(PrescriptionStateEnum.Running));
 		pc.edit();
+		// after pc.edit() we go to the editi View, the status of prescription should = Edit
 		GregorianCalendar endDate = new GregorianCalendar();
-		endDate.set(2001, 01, 01);
+		endDate.set(2001, 1, 1, 0, 0, 0);
 		prescription.setEndDate(endDate);
-		assertTrue("Staus im EditierenView auf Edit",prescription.getPrescriptionState().equals(PrescriptionStateEnum.Edit));
-		assertTrue("Datum wurde über prescription auf 01.01.2001 korrekt geändert", prescription.getEndDate().equals(endDate));
+		assertTrue("Staus im EditierenView ist nicht auf Edit",prescription.getPrescriptionState().equals(PrescriptionStateEnum.Edit));
+		assertTrue("Datum wurde über prescription auf 01.01.2001 nicht korrekt geändert", prescription.getEndDate().equals(endDate));
 		pc.save();
-//		assertTrue("EndDatum wurde überschritten und somit ist State auf Ended",prescriptionRepo.getById(prescription.getPrescriptionId()).getEndDate().equals(PrescriptionStateEnum.Ended));
-//		assertTrue("EndDatum wurde überschritten und somit ist State auf Ended",prescription.getPrescriptionState().equals(PrescriptionStateEnum.Ended));
-//		assertTrue("Datum wurde über prescriptionRepo.getById auf 01.01.2001 korrekt geändert",prescriptionRepo.getById(prescription.getPrescriptionId()).getEndDate().equals(endDate));
+		
+		// after pc.save() should the State of the prescription = Ended. Because the endDate is expired!
+		//	We have an assert because we haven't validate the Dates 
+		assertTrue("EndDatum wurde überschritten und State ist nicht auf Ended",prescriptionRepo.getById(prescription.getPrescriptionId()).getEndDate().equals(PrescriptionStateEnum.Ended));
+		assertTrue("EndDatum wurde überschritten und State ist nicht auf Ended",prescription.getPrescriptionState().equals(PrescriptionStateEnum.Ended));
+		assertTrue("Datum wurde über prescriptionRepo.getById auf 01.01.2001 nicht korrekt geändert",prescriptionRepo.getById(prescription.getPrescriptionId()).getEndDate().equals(endDate));
 		pc.delete();
-		assertTrue("OK",prescription.getPrescriptionState().equals(PrescriptionStateEnum.Deleted));
 		prescriptionRepo.remove(prescriptionRepo.getById(Prescription.class, pc.getPrescription().getPrescriptionId()));
-	}
+		assertTrue("OK",prescription.getPrescriptionState().equals(PrescriptionStateEnum.Deleted));
+		}
 	
 	
 }
